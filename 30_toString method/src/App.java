cc9a677d8bb598969d545ef2acf8b96eb1470645
AungKaung <103877301+AungKaung1928@users.noreply.github.com>
2024-01-28 19:59:58 +0900

    Add files via upload

---
+++ 30_toString method/src/App.java
@@ -0,0 +1,22 @@
+
+public class App {
+
+	public static void main(String[] args) {
+
+		// toString() = special method that all objects inherit, 
+		//			    that returns a string that "textually represents" an object.
+		//				can be used both implicitly and explicitly
+		
+		
+		Car car = new Car();
+		
+		//explicit
+//		System.out.println(car.toString());
+		
+		//implicit
+		System.out.println(car);
+		
+		
+	}
+
+}
---
+++ 30_toString method/src/Car.java
@@ -0,0 +1,16 @@
+
+public class Car {
+
+
+		String make = "Ford";
+		String model = "Mustang";
+		String color = "green";
+		int year = 2024;
+		
+		public String toString() {
+			
+			return make + "\n" + model +   "\n"  + color +  "\n" + year;
+			
+			
+	}
+}
